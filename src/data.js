6d3b20eb916b3c441123f1ecbfb7328323832733
Tyler Hooks <tylerhooks8@gmail.com>
2021-09-13 23:43:13 -0500

    portfolio site

---
+++ src/data.js
@@ -0,0 +1,32 @@
+export const projects = [
+{
+    title: "Pitch App", 
+    subtitle: "NSBE LinkedIn Hackathon Project", 
+    description: `We are all familiar with the brief speech that happens within 20 to 30 seconds in professional environments 
+                  called an elevator pitch. An elevator pitch is a simple and direct opportunity for job 
+                  seekers to share their skills more engagingly. Pitch would be a short-form video platform 
+                  for recruiters to seek out potential employees aside from viewing just resumes and profiles.`, 
+    image: "../photos/pitch.jpg",
+    link: "https://pitch30.community/"
+  }, 
+  {
+    title: "Gulp Project Seed", 
+    subtitle: "Seed file for web development projects", 
+    description: `I created this project seed as a way to gain better understand of the Gulp task manager. In addition, it has been
+    a great way to start of the development process for freelance and personal projects.`, 
+    image: "../photos/pitch.jpg",
+    link: "https://github.com/tylerhooks1/mygulp"
+  }, 
+
+]; 
+
+export const skills = [
+        "HTML/CSS",
+        "JavaScript", 
+        "React.js",
+        ".Net", 
+        "Bootstrap", 
+        "Node.js", 
+        "C++", 
+        "Java"
+];
