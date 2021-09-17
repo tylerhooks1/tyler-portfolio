678dae14eeb1102e9bc0925026c8e8e9521fbe00
Tyler Hooks <tylerhooks8@gmail.com>
2021-09-17 16:45:25 -0500

    more form updates

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -62,7 +62,7 @@ export default function Contact() {
           method="POST"
           data-netlify="true"
           className="xl:w-2/5 lg:w-1/3 md:w-1/2 flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0">
-          <input type="hidden" name="form-name" value="contact" />
+          <input type="hidden" name="contact" value="contact" />
           <h2 className="sm:text-4xl text-3xl mb-1 font-medium title-font">
             Let's Connect
           </h2>
