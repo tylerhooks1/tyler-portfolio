cc450a009536d3c2a1afa558421453fcdfd94bab
Tyler Hooks <tylerhooks8@gmail.com>
2021-09-17 09:56:19 -0500

    minor spacing updates to About and and Contact

--- src/components/About.js
+++ src/components/About.js
@@ -6,33 +6,33 @@ import Github from "../photos/github.svg";
 export default function About() {
   return (
     <section className="my-20" id="about">
-      <div className="container mx-auto px-10 mx-10 md:pb-20 flex items-center justify-between">
-        <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
+      <div className="container mx-auto px-10 mx-10 lg:pb-20 flex items-center justify-between">
+        <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-12 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
           <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium">
             Hello, my name is Tyler Hooks.
             <br className="hidden lg:inline-block" /> I'm a Software Engineer
-            <br className="hidden lg:inline-block" />with interests in React Development and UI engineering.
+            <br className="hidden lg:inline-block" /> with interests in React Development and UI engineering.
           </h1>
-          <div className="flex mb-4">
-          <a href="https://www.linkedin.com/in/tyler-hooks-9214abb5" className="w-5 mr-4">
+          <div className="flex mb-4 items-center">
+          <a href="https://www.linkedin.com/in/tyler-hooks-9214abb5" className="w-6 mr-4">
             <img
               src={Linkedin}
             />
           </a>
-          <a href="https://github.com/tylerhooks1" className="w-5">
+          <a href="https://github.com/tylerhooks1" className="w-6">
             <img
               src={Github}
             />
           </a>
           </div>
-          <p className="mb-8 leading-relaxed lg:w-2/3 mb-2">
+          <p className="mb-8 leading-relaxed mb-2">
             I have over 2 years of experience with JavaScript frameworks and applications, web development, and the server-side languages Java and .NET.
             In my current role, I manage a UI component library that has resulted in an increase in compatibility, accessibility, and efficiency of hundreds of projects in production.
             Additionally, I have collaborated with UX designers to develop components and build out front-end applications across my organization. 
           </p>
 
           <h2 className="text-xl font-medium">About this portfolio </h2>
-          <p className="mb-8 leading-relaxed lg:w-2/3">
+          <p className="mb-8 leading-relaxed">
             This portfolio site was implemented using React.js,Tailwind CSS for styling, and deployed with Netlify. <br />
             Please checkout the <span><a class="text-burntorange" href="#">Git repository</a></span> for more information.
             In time, I plan on adding more projects to this site and can't wait to share what I've learned and the many things I'm passionate about!
@@ -50,7 +50,7 @@ export default function About() {
             </a>
           </div>
         </div>
-        <div className="hidden md:block lg:max-w-xl lg:w-full md:w-1/2 w-5/6">
+        <div className="hidden md:block xl:max-w-xl xl:w-full md:w-1/2 w-5/6">
           <img
             src={Hero}
           />
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -58,7 +58,7 @@ export default function Contact() {
         <form
           netlify
           name="contact"
-          className="lg:w-1/3 md:w-1/2 flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0">
+          className="xl:w-2/5 lg:w-1/3 md:w-1/2 flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0">
           <h2 className="sm:text-4xl text-3xl mb-1 font-medium title-font">
             Let's Connect
           </h2>
