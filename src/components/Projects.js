a1f96c3b2e93a859a0dc520b3362bc17aadffcb0
Tyler Hooks <tylerhooks8@gmail.com>
2021-09-24 14:35:47 -0500

    updated project links target

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -20,6 +20,7 @@ export default function Projects() {
                     {projects.map((project) => (
                         <a
                             href={project.link}
+                            target="_blank"
                             key={project.description}
                             className="sm:w-1/2 w-100 p-4">
                             <div className="flex relative">
